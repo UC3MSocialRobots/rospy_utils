276e63dec9cfd5011bd42eb552677de45c04b7f5
VGonPa <victor.gonzalez.pacheco@gmail.com>
2015-01-11 21:43:29 +0000

    ASCII encoding

--- src/rospy_utils/iter_utils.py
+++ src/rospy_utils/iter_utils.py
@@ -1,20 +1,22 @@
-# :version:      0.1
+
+# :version:      0.1.0
 # :copyright:    Copyright (C) 2014 Universidad Carlos III de Madrid.
 #                Todos los derechos reservados.
-# :licencia      LASR_UC3M v1.0, ver LICENCIA.txt
+# :license       LASR_UC3M v1.0, ver LICENCIA.txt
 
 # Este programa es software libre: puede redistribuirlo y/o modificarlo
-# bajo los términos de la Licencia Académica Social Robotics Lab - UC3M
-# publicada por la Universidad Carlos III de Madrid, tanto en su versión 1.0
-# como en una versión posterior.
+# bajo los terminos de la Licencia Academica Social Robotics Lab - UC3M
+# publicada por la Universidad Carlos III de Madrid, tanto en su version 1.0
+# como en una version posterior.
 
-# Este programa se distribuye con la intención de que sea útil,
-# pero SIN NINGUNA GARANTÍA. Para más detalles, consulte la
-# Licencia Académica Social Robotics Lab - UC3M versión 1.0 o posterior.
+# Este programa se distribuye con la intencion de que sea util,
+# pero SIN NINGUNA GARANTIA. Para mas detalles, consulte la
+# Licencia Academica Social Robotics Lab - UC3M version 1.0 o posterior.
 
-# Usted ha recibido una copia de la Licencia Académica Social
-# Robotics Lab - UC3M en el fichero LICENCIA.txt, que también se encuentra
+# Usted ha recibido una copia de la Licencia Academica Social
+# Robotics Lab - UC3M en el fichero LICENCIA.txt, que tambien se encuentra
 # disponible en <URL a la LASR_UC3Mv1.0>.
+
 
 '''
     @author: Victor Gonzalez Pacheco (victor.gonzalez.pacheco at gmail.com)
